bb0a43d959e8e1808d9913f4dab0b3e7f1e94864
L0G1C06 <justgithub70@gmail.com>
2025-05-30 19:42:50 -0300

    updating code to Azure build

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -9,8 +9,8 @@ const options = {
       description: 'Documentação da API com Swagger',
     },
     servers: [
-      //{ url: 'https://bblackbox-f3btf4c3g7fydhaf.westus-01.azurewebsites.net' }
-      { url: 'http://localhost:3000' }
+      { url: 'https://bblackbox-f3btf4c3g7fydhaf.westus-01.azurewebsites.net' }
+      //{ url: 'http://localhost:3000' }
     ],
     components: {
       securitySchemes: {
